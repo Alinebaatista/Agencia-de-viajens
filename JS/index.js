8e47a9eca86822636647d5c50e21a6d847f8e2e6
Aline Batista <102964288+Alinebaatista@users.noreply.github.com>
2023-01-02 22:19:13 -0300

    Update index.js
    a

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+var Opinion =  document.getElementById("Opinion"); // id ->  È uma chave de comunicação com o js , getElemnte puxa.
+function opiniao(){
+    var x;
+    var nome;
+
+    var nome=prompt("Digite seu Nome"); 
+    var x = prompt(")
+
+        alert(" Olá " + nome + " Você deu " + x + " estrelas \n Muito Obrigado!");// mostrar o que o usuario digitou
+
+}
